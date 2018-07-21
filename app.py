33cfc9eadbe954800119c05ea9ba0a86aff3e87e
Tayyab Hussain <tayyabmh93@gmail.com>
2018-07-21 11:04:14 -0700

    init

---
+++ app.py
@@ -0,0 +1,89 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+import re
+import math
+from functools import reduce
+
+# Instantiating Flask app and API
+app = Flask(__name__)
+api = Api(app)
+
+# Takes JSON request with "numbers" converts it to an array and checks to see if there are only numbers
+# Returns a str -> float number list for numbers to be calculated on
+# If not it returns back an error
+def json_numbers_validation():
+    json_data = request.get_json(force=True)
+    number_string = str(json_data["numbers"])
+    number_list = re.split(',|\s', number_string)
+    number_list = filter(None, number_list)
+
+    if (all (i.isdigit() for i in number_list)) == False:
+        return False,"Hey, seems like you used either a non-comma or non-numeric value. Please try again."
+    else:
+        float_list = [float(i) for i in number_list]
+        return True, float_list
+
+# Multiply class unpacks return variables from json_numbers_validation()
+# Performs calculation if the numbers were valid, checks for any overflow errors
+# Then return response as string
+class Multiply(Resource):
+    def post(self):
+        is_valid, validation_return_body = json_numbers_validation()
+        if is_valid == False:
+            return  validation_return_body
+        else:
+            float_list = validation_return_body
+            result = reduce((lambda x,y: x * y), float_list)
+            if math.isinf(result):
+                return "Woah those were those large numbers, looks like we had an overflow error! Try again with smaller/less numbers."
+            return "Multiplication results in: {}".format(result)
+
+# Divide class/resource is similar to multiply except there has been an added div/0 error check
+class Divide(Resource):
+    def post(self):
+        is_valid, validation_return_body = json_numbers_validation()
+        if is_valid == False:
+            return  validation_return_body
+        else:
+            float_list = validation_return_body
+            if (0 in float_list):
+                return "Hey come on man we know you can't divide by zero! Please try again."
+            result = reduce((lambda x,y: x / y), float_list)
+
+            return "Division results in: {}".format(result)
+
+# Similar to Multiply Resource
+class Add(Resource):
+    def post(self):
+        is_valid, validation_return_body = json_numbers_validation()
+        if is_valid == False:
+            return  validation_return_body
+        else:
+            float_list = validation_return_body
+            result = reduce((lambda x,y: x + y), float_list)
+            if math.isinf(result):
+                return "Woah those were those large numbers, looks like we had an overflow error! Try again with smaller/less numbers"
+            return "Addition results in: {}".format(result)
+
+# Similar to Multiply Resource
+class Subtract(Resource):
+    def post(self):
+        is_valid, validation_return_body = json_numbers_validation()
+        if is_valid == False:
+            return  validation_return_body
+        else:
+            float_list = validation_return_body
+            result = reduce((lambda x,y: x - y), float_list)
+            if math.isinf(result):
+                return "Woah those were those large numbers, looks like we had an overflow error! Try again with smaller/less numbers"
+            return "Subtraction results in: {}".format(result)
+
+# Adding resources to the API
+api.add_resource(Multiply,'/multiply')
+api.add_resource(Divide,'/divide')
+api.add_resource(Add,'/add')
+api.add_resource(Subtract,'/subtract')
+
+# Run the app when ready
+if __name__ == '__main__':
+    app.run(debug=True)
